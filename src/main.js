43d23f573ae71c7c1e406c8cc7586fa8a270e931
Ko Pin <kopin88@gmail.com>
2019-11-05 02:07:21 +0630

    -- add dependencies

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import './plugins/axios'
+import './plugins/fontawesome'
 import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
